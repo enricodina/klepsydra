3e3203430e92dab17bb26cb9ccd1ade1a478377f
enricodina <enricodina@hotmail.com>
2024-05-12 17:35:37 +0400

    renamed

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Klepsydra Jaume Codina Mas',
+      title: 'Klepsydra',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
---
+++ lib/todolist.dart
@@ -0,0 +1,4 @@
+
+// 1 implement proper filters and organiser on filter tab
+// 2 upload proper tiker lists for chart tab
+// 3 add sorting parametres on filter tab
